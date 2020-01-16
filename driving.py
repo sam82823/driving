55be47c3302dc352b1103dd6a63b50f7637b93af
Sam Li <sam82823@gmail.com>
2020-01-16 19:32:21 +0800

    add only taiwan or US

--- driving.py
+++ driving.py
@@ -1,7 +1,7 @@
 country = input('What is your contury: ')
 age = input('How old are you: ')
 age = int(age)
-if country == 'Taiwan' or 'taiwan':
+if country == 'Taiwan' or country == 'taiwan':
 	if age >= 18:
 		print('you can drive')
 	else:
@@ -11,3 +11,5 @@ elif country == 'US':
 		print('you can drive')
 	else:
 		print('NO drive')
+else:
+	print('only Taiwan or US')
